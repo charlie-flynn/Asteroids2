f7bee0dcede138ee0a0bd8c2e9a4f60e9104b4e6
charlie-flynn <charlie.flynn.business@gmail.com>
2024-11-11 16:51:50 -0600

    added SpawnStuff function because im not putting that in update good god theres so much code

--- Asteroids2/src/Game/GameScene.cs
+++ Asteroids2/src/Game/GameScene.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Raylib_cs;
 using MathLibrary;
+using System.Security.Cryptography;
 
 namespace Asteroids2
 {
@@ -28,14 +29,15 @@ namespace Asteroids2
 
             if (_spawnTimer <= 0.0)
             {
-                Actor.Instantiate(new Asteroid(50, 10), null, new Vector2(), 0);
-                _spawnTimer = 20.0 - (_difficultyModifier * 2);
+                SpawnStuff(6);
+
+                _spawnTimer = 5 - (_difficultyModifier * 2);
 
                 if (_spawnTimer < 5.0)
                     _spawnTimer = 5.0;
             }
 
-            _difficultyModifier = (int)_score / 10000;
+            _difficultyModifier = (int)_score / 50000;
 
             _spawnTimer -= deltaTime;
 
@@ -45,6 +47,48 @@ namespace Asteroids2
         private void OnAsteroidKill(float radius)
         {
             _score += (int)(radius * 100);
+        }
+
+        private void SpawnStuff(int amount)
+        {
+            // spawn all of the asteroids
+            for (int i = 0; i < amount; i++)
+            {
+                int rng = RandomNumberGenerator.GetInt32(1, 101);
+                Vector2 randomPosition;
+
+                // decide which side to put the asteroid on
+                if (rng < 25)
+                {
+                    randomPosition = new Vector2(0, Raylib.GetScreenHeight() / (rng / 4 + .0001f));
+                }
+                else if (rng < 50)
+                {
+                    randomPosition = new Vector2(Raylib.GetScreenWidth(), Raylib.GetScreenHeight() / (rng / 4));
+                }
+                else if (rng < 75)
+                {
+                    randomPosition = new Vector2(Raylib.GetScreenWidth() / (rng / 4), 0);
+                }
+                else
+                {
+                    randomPosition = new Vector2(Raylib.GetScreenWidth() / (rng / 4), Raylib.GetScreenHeight());
+                }
+
+                Vector2 center = new Vector2(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() / 2);
+                Vector2 spawnToCenterDirection = (randomPosition - center).Normalized;
+
+                // spawn the asteroid
+                // TO DO: actually make it go towards the center kinda
+                Actor.Instantiate(new Asteroid(50, 15), null, randomPosition, spawnToCenterDirection.Angle(new Vector2(1, 0)) * (180 / (float)Math.PI));
+
+                rng = RandomNumberGenerator.GetInt32(1, 21);
+                if (rng == 1)
+                {
+                    Actor.Instantiate(new Friend(), null, randomPosition, 0);
+                }
+            }
+
         }
     }
 }
