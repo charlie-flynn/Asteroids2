fbb15677cc9da4175a507fc123eabf67014d78f8
charlie-flynn <charlie.flynn.business@gmail.com>
2024-11-12 16:08:40 -0600

    removed something that never existed and wrote the Worst Line Of Code Ever Made (Please Destroy It When Youre DOne With It)

--- Asteroids2/src/Game/GameScene.cs
+++ Asteroids2/src/Game/GameScene.cs
@@ -29,7 +29,7 @@ namespace Asteroids2
 
             if (_spawnTimer <= 0.0)
             {
-                SpawnStuff(6);
+                SpawnStuff(1);
 
                 _spawnTimer = 5 - (_difficultyModifier * 2);
 
@@ -42,6 +42,10 @@ namespace Asteroids2
             _spawnTimer -= deltaTime;
 
             Raylib.DrawText(_score.ToString(), 10, 10, 20, Color.Red);
+
+            if (lastAsteroidSpawnDirection != null)
+                Raylib.DrawLineV(new Vector2(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() / 2), new Vector2(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() / 2) + lastAsteroidSpawnDirection * 50, Color.Blue);
+                Raylib.DrawLineV(new Vector2(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() / 2), new Vector2(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() / 2) + new Vector2(1, 0) * 50, Color.Blue);
         }
 
         private void OnAsteroidKill(float radius)
@@ -60,27 +64,30 @@ namespace Asteroids2
                 // decide which side to put the asteroid on
                 if (rng < 25)
                 {
-                    randomPosition = new Vector2(0, Raylib.GetScreenHeight() / (rng / 4 + .0001f));
+                    randomPosition = new Vector2(0, Raylib.GetScreenHeight() / (((rng + .0001f) % 25) / 4));
                 }
                 else if (rng < 50)
                 {
-                    randomPosition = new Vector2(Raylib.GetScreenWidth(), Raylib.GetScreenHeight() / (rng / 4));
+                    randomPosition = new Vector2(Raylib.GetScreenWidth(), Raylib.GetScreenHeight() / (((rng + .0001f) % 25) / 4));
                 }
                 else if (rng < 75)
                 {
-                    randomPosition = new Vector2(Raylib.GetScreenWidth() / (rng / 4), 0);
+                    randomPosition = new Vector2(Raylib.GetScreenWidth() / (((rng + .0001f) % 25) / 4), 0);
                 }
                 else
                 {
-                    randomPosition = new Vector2(Raylib.GetScreenWidth() / (rng / 4), Raylib.GetScreenHeight());
+                    randomPosition = new Vector2(Raylib.GetScreenWidth() / (((rng + .0001f) % 25) / 4), Raylib.GetScreenHeight());
                 }
 
                 Vector2 center = new Vector2(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() / 2);
-                Vector2 spawnToCenterDirection = (randomPosition - center).Normalized;
+                Vector2 spawnToCenterDirection = ((center - randomPosition) * -1).Normalized;
+                Console.WriteLine(rng.ToString());
+                lastAsteroidSpawnDirection = spawnToCenterDirection;
 
                 // spawn the asteroid
                 // TO DO: actually make it go towards the center kinda
-                Actor.Instantiate(new Asteroid(50, 15), null, randomPosition, spawnToCenterDirection.Angle(new Vector2(1, 0)) * (180 / (float)Math.PI));
+                Actor.Instantiate(new Asteroid(50, 15), null, randomPosition, -spawnToCenterDirection.Angle(new Vector2(1, 0)) * (180 / (float)Math.PI));
+                Console.WriteLine((spawnToCenterDirection.Angle(new Vector2(1, 0)) * (180 / (float)Math.PI)).ToString());
 
                 rng = RandomNumberGenerator.GetInt32(1, 21);
                 if (rng == 1)
