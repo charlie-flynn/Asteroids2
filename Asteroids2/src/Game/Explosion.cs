7c4d6d9246f051e1c80a535d1aebb248212f818c
charlie-flynn <charlie.flynn.business@gmail.com>
2024-11-22 10:56:00 -0600

    added a few comments

--- Asteroids2/src/Game/Explosion.cs
+++ Asteroids2/src/Game/Explosion.cs
@@ -24,12 +24,14 @@ namespace Asteroids2
         {
             base.Update(deltaTime);
 
+            // decrease the radius by the dissipation speed multiplied by deltaTime
             _radius -= _dissipationSpeed * deltaTime;
-
             Transform.LocalScale = new Vector2((float)_radius, (float)_radius);
 
+            // draw
             Raylib.DrawCircleV(Transform.GlobalPosition, Transform.GlobalScale.x, Color.Orange);
 
+            // if the radius is less than zero, destroy this for it has no further purpose
             if (_radius <= 0.0)
                 Destroy(this);
         }
